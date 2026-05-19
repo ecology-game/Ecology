9e3e2f136ac73e4ff337ae1eb10f8e2aa51d258c
ecology-game <244162703+ecology-game@users.noreply.github.com>
2026-05-19 11:15:04 +0300

    Update ho.js

--- ho.js
+++ ho.js
@@ -51,10 +51,7 @@ const plants = [
     name:"🍋 Lemon Plant",
     info:"Lemon plants grow citrus fruits rich in vitamin C."
   }
-{
-    name:"🌷 Tulip",
-    info:"Tulips are colorful spring flowers known for their soft petals and elegant shape."
-  },
+
 ];
 
 // CAPTURE PHOTO
